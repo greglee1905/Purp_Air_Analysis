815987c74bc51f605e12830bc7c45f6ace66b865
greglee1905 <greglee1905@aol.com>
2020-04-08 17:19:35 -0600

    edits to airflow

--- purp_air.py
+++ purp_air.py
@@ -21,11 +21,7 @@ def purp_air():
     features = query.results.apply(pd.Series)
     raw_data = query[['mapVersion','baseVersion','mapVersionString']]
     query = pd.concat([raw_data,features],axis =1)
-<<<<<<< HEAD
     query.to_pickle('/home/boogie2/external/tiny_tower_2/purpleair_data/legacy_' + str(t) + '.pkl')
-=======
-    query.to_pickle('/home/boogie2/airflow/purpleair_data/legacy_' + str(t) + '.pkl')
->>>>>>> 5994faf4348423cc324d7d26bff35cb8a0c640f9
 
     return 'legacy data acquired'
 
@@ -36,7 +32,6 @@ def purp_air_exp():
     t = now.strftime("%m_%d_%H_%M")
 
     #Experimental Data
-<<<<<<< HEAD
     url = "https://www.purpleair.com/data.json"
     exp = json.load(urlopen(url))
 
@@ -44,42 +39,22 @@ def purp_air_exp():
     exp_df['version'] = exp['version']
     exp_df['count']= exp['count']
     exp_df.to_pickle('/home/boogie2/external/tiny_tower_2/purpleair_data/experimental_' + str(t) + '.pkl')
-=======
-    exp_url = "https://www.purpleair.com/data.json"
-    exp_data = json.load(urlopen(url))
-
-    query = pd.DataFrame(data)
-    features = query.results.apply(pd.Series)
-    raw_data = query[['mapVersion','baseVersion','mapVersionString']]
-    exp_query = pd.concat([raw_data,features],axis =1)
-    exp_query.to_pickle('/home/boogie2/airflow/purpleair_data/experimental_' + str(t) + '.pkl')
->>>>>>> 5994faf4348423cc324d7d26bff35cb8a0c640f9
 
     return 'experimental data acquired'
 
 #Airflow stuff
 default_args = {
     'owner': 'airflow',
-<<<<<<< HEAD
     'start_date': dt.datetime(2020, 4, 6, 21, 18, 00),
     'concurrency': 1,
     'retries': 3,
     'retry_delay': dt.timedelta(seconds = 60)
-=======
-    'start_date': dt.datetime(2020, 4, 5, 3, 00, 00),
-    'concurrency': 1,
-    'retries': 0
->>>>>>> 5994faf4348423cc324d7d26bff35cb8a0c640f9
 }
 
 with DAG('purp_air',
          catchup=False,
          default_args=default_args,
-<<<<<<< HEAD
          schedule_interval='@hourly'
-=======
-         schedule_interval='*/60 * * * *',
->>>>>>> 5994faf4348423cc324d7d26bff35cb8a0c640f9
          ) as dag:
 
     opr_purp_air = PythonOperator(task_id='legacy',
@@ -87,9 +62,3 @@ with DAG('purp_air',
 
     opr_purp_air_exp = PythonOperator(task_id='experimental',
                                python_callable=purp_air_exp)
-<<<<<<< HEAD
-=======
-
-
-opr_purp_air >> opr_purp_air_exp
->>>>>>> 5994faf4348423cc324d7d26bff35cb8a0c640f9
